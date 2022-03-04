d180b98448a400d7238b14d793f942ff01874fce
GlacialStudio <aclearmonth@gmail.com>
2022-03-04 18:17:57 +0000

    Fresh laravel for dev

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@home');
-Route::get('/events', 'PagesController@events');
-Route::get('/events/{filter}', 'PagesController@events');
-
-/*
 Route::get('/', function () {
     return view('welcome');
 });
-*/
